76147d8659ddaf47b0740cc4f4cc59f1457f013f
Romulo Fernando <fssromulo@gmail.com>
2023-02-23 06:27:31 -0300

    Added filter option on Title component

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,13 +1,49 @@
 <template>
     <div class="title-wrapper">
         {{ title }}
+        <select v-model="filter" v-if="shouldShowFilters">
+            <option v-for="filterOpt in filtersOptions" :value="filterOpt.value">{{ filterOpt.label }}</option>
+        </select> --> Filter Value: {{ filter }}
+
     </div>
 </template>
 
-<script setup lang="ts">
-defineProps<{
-    title: string;
-}>();
+<script lang="ts">
+
+import { defineComponent, ref, type Ref } from 'vue';
+
+enum EFiltersOption {
+    NONE = 'NONE',
+    STARTS = 'STARTS',
+    FORKS = 'FORKS',
+    HELPED_ISSUES = 'HELPED_ISSUES',
+    UPDATED = 'UPDATED',
+}
+
+export default defineComponent({
+    props: {
+        title: { type: String, required: true, default: '' },
+        shouldShowFilters: { type: Boolean, required: false, default: false },
+    },
+
+    setup() {
+        const filter: Ref<string> = ref(EFiltersOption.NONE);
+
+        const filtersOptions: Array<{ value: string, label: string }> = [
+            { label: 'No filter apply', value: EFiltersOption.NONE },
+            { label: 'Sort by stars', value: EFiltersOption.STARTS },
+            { label: 'Sort by forks', value: EFiltersOption.FORKS },
+            { label: 'Sort by help wanted issues', value: EFiltersOption.HELPED_ISSUES },
+            { label: 'Sort by updated', value: EFiltersOption.UPDATED }
+        ]
+
+        return {
+            filter,
+            filtersOptions
+        }
+    },
+
+});
 </script>
 
 <style scoped>
--- src/views/DiscoveryView.vue
+++ src/views/DiscoveryView.vue
@@ -2,7 +2,15 @@
 	<div class="username">
 		<h1>DiscoveryView</h1>
 		<Title title="My Bookmarks"></Title>
-		<RepositoriesList :repositories="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]" />
+		<RepositoriesList :repositories="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]" />
+
+		<hr />
+		<h3>Aqui vai o "tooglle topics to show"</h3>
+		<hr />
+
+		<Title title="Top vue" :should-show-filters="true"></Title>
+		<RepositoriesList :repositories="[11, 22, 33, 45, 55, 66, 77, 88, 95, 99]" />
+
 	</div>
 </template>
 
